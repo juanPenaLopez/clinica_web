c43b469629a03948e1b8dbe5820b8383dfa7bcbc
juanPenaLopez <juanp.penal@uqvirtual.edu.co>
2023-11-09 23:03:13 -0500

    Se crea formulario de registro

--- src/app/pagina/registro/registro.component.ts
+++ src/app/pagina/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {RegistroPacienteDTO} from "../../modelo/registro-paciente-dto";
 
 @Component({
   selector: 'app-registro',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class RegistroComponent {
 
+  public registroPacienteDTO: RegistroPacienteDTO;
+  constructor(){
+    this.registroPacienteDTO = new RegistroPacienteDTO();
+  }
+
+  public registrar(){
+    console.log(this.registroPacienteDTO);
+  }
+
+  public sonIguales():boolean{
+    return this.registroPacienteDTO.password == this.registroPacienteDTO.confirmaPassword;
+  }
 }
